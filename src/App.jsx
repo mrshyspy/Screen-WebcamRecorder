28e552ddf77ad52f2e21a9c6c20842f9f7cbeb22
sohil khan <skb90011@gmail.com>
2025-08-25 20:57:36 +0530

    added images

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,13 @@ import React from "react";
 import Home from "./pages/home";
 import RecordedVideo from "./pages/RecordedVideo";
 // import VideoEditorPage from './pages/VideoEditorPage';
-import LoginPage from "./pages/LoginPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Signin from "./pages/Signin";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
-                <Route path="/Signin" element={<Signin />} />
-
 
         <Route path="/RecordedVideo" element={<RecordedVideo />} />
         {/* <Route path="/VideoEditorPage" element={<VideoEditorPage />} /> */}
