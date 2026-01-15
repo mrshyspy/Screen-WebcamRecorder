32761b28d7a94091b7f2be29413751894fe472a5
sohil khan <skb90011@gmail.com>
2026-01-15 11:54:45 +0530

    initial commit (without secrets)

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Home from "./pages/home";
 import RecordedVideo from "./pages/RecordedVideo";
 // import VideoEditorPage from './pages/VideoEditorPage';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Portfolio from "./pages/portfolio";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Portfolio" element={<Portfolio />} />
         <Route path="/RecordedVideo" element={<RecordedVideo />} />
         {/* <Route path="/VideoEditorPage" element={<VideoEditorPage />} /> */}
       </Routes>
